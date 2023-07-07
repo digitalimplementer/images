b0cd8ee3acf144ce993dabdc87d2c5ec0db9948e
digitalimplementer <maksym.pavlichenko.cubex@gmail.com>
2023-07-07 13:26:20 +0300

    update description

--- frontend/src/pages/intro/Intro.tsx
+++ frontend/src/pages/intro/Intro.tsx
@@ -11,10 +11,10 @@ const Intro = () => {
       <div className="content">
         <h1>Images</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
-          eum est! Impedit quod, culpa, non vero asperiores, nesciunt dolor
-          soluta quidem consequuntur quis quibusdam. Quas nam eaque velit nisi?
-          Voluptates?
+          Here you can create a gallery of your photos. First, you need to
+          upload them, add a description if you wish, and you can also delete
+          them. clicking on a photo, you will be able to view the photo in full
+          size and switch between them using the prev and next buttons
         </p>
         <ButtonComponent
           text={"Next"}
